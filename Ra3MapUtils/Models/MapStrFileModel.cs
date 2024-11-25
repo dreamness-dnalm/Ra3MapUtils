95faa4441ad98811cad0e08635b3e48829425660
dreamness <dreamness@163.com>
2024-11-25 18:43:48 +0800

    upload

---
+++ Ra3MapUtils/Models/MapStrFileModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ra3MapUtils.Models;
+
+public partial class MapStrFileModel: ObservableObject
+{
+    [ObservableProperty] private ObservableCollection<MapStrFileRecordModel> _showingRecords = new();
+    
+    [ObservableProperty] private Dictionary<string, string> _recordDict = new();
+
+    [ObservableProperty] private string _mapShowingName = "";
+}
+
+public partial class MapStrFileRecordModel : ObservableObject
+{
+    
+}
