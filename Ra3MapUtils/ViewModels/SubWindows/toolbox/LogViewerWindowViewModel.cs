2693f1d87832ebb8181d37b0a9aca256e2d693d5
dreamness <dreamness@163.com>
2025-02-13 02:53:44 +0800

    upload

--- Ra3MapUtils/ViewModels/SubWindows/toolbox/LogViewerWindowViewModel.cs
+++ Ra3MapUtils/ViewModels/SubWindows/toolbox/LogViewerWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Windows.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,5 +12,57 @@ namespace Ra3MapUtils.ViewModels.toolbox;
 
 public partial class LogViewerWindowViewModel: ObservableObject
 {
+    [ObservableProperty] private int _logLevelIndex = 1;
+
+    [ObservableProperty] private string _logFileName = "aaa";
+
+    [ObservableProperty] private string _parseStatus = "已停止";
     
+    [ObservableProperty] private Brush _parseStatusColor = Brushes.Red;
+
+    private string logLevel = "DEBUG";
+
+    private long pos = 0;
+
+    [RelayCommand]
+    private void StartFromBeginning()
+    {
+        
+    }
+
+    [RelayCommand]
+    private void StartFromLatest()
+    {
+        
+    }
+
+    [RelayCommand]
+    private void StartFromLastStopPos()
+    {
+        
+    }
+
+    [RelayCommand]
+    private void Stop()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void cleanLog()
+    {
+        
+    }
+
+    [RelayCommand]
+    private void SwitchLogFile()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void SwitchLogLevel()
+    {
+        MessageBox.Show("SwitchLogLevel: " + _logLevelIndex);
+    }
 }
