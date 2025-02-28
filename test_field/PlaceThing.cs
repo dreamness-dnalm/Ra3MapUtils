e0518563607abb3ac8ebbcd764a447c0bf2a6594
dreamness <dreamness@163.com>
2025-02-28 10:28:54 +0800

    upload

---
+++ test_field/PlaceThing.cs
@@ -0,0 +1,28 @@
+﻿using MapCoreLib.Core;
+using MapCoreLib.Core.Asset;
+using MapCoreLib.Core.Scripts;
+using MapCoreLib.Util;
+
+namespace test_field;
+
+public class PlaceThing
+{
+    public class Main: ScriptInterface
+    {
+        public void Apply(MapDataContext mapDataContext)
+        {
+            ObjectsList objectsList = mapDataContext.getAsset<ObjectsList>(Ra3MapConst.ASSET_ObjectsList);
+            
+            var heightMapData = mapDataContext.getAsset<HeightMapData>(Ra3MapConst.ASSET_HeightMapData);
+
+            var random = new Random();
+            for (int i = 0; i < 700; i++)
+            {
+                objectsList.AddObject(mapDataContext, 
+                    "HA_Tree01",
+                    new Vec3D(mapDataContext.mapWidth * 10 * random.NextDouble(), mapDataContext.mapHeight * 10 * random.NextDouble()),
+                    45);
+            }
+        }
+    }
+}
